58d5b39a7821c2195f1dc061cc08de1dda58340b
Patrick Isaiah <patrick21isaiah@gmail.com>
2021-04-26 17:00:09 +0100

    first commit

---
+++ src/components/HowToStaySafe/HowToStaySafe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from './HowToStaySafe.module.css'
+
+function HowToStaySafe() {
+  return (
+    <div className={styles.staysafe}>
+      <h2>How to reduce transmission ?</h2>
+      <p>Wear a mask...</p>
+      <p>Maintain social distancing...</p>
+      <p>Wash and sanitise your hands regularly...</p>
+    </div>
+  )
+}
+
+export default HowToStaySafe
